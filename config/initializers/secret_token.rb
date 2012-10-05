2227a6118a91efb63aa7bdfc350f59e3184369f6
zxyviopond <zxyviopond@gmail.com>
2012-10-04 23:54:09 -0700

    Test Github working for my ruby

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'fed38713f42df2ebd863eb9bdb93096cf8841c244c61c36130ffd10f0fbed0a48e3c40be980c5dc867864d46abf25670da3a1093a78091b614a2f08afadffccb'
